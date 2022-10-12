2d2dad43cb5958448c65e0db0c1db06a4d794107
vanya_ttoneva@students.softuni.bg <vanya_ttoneva@students.softuni.bg>
2022-10-12 21:16:06 +0300

    Add project files.

---
+++ Passengers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Passengers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons =
+                Console.ReadLine().
+                Split().
+                Select(int.Parse).
+                ToList();
+            int maxPassengers = int.Parse(Console.ReadLine());
+            string comm = Console.ReadLine();
+
+            while(comm != "end")
+            {
+                string[] command = comm.Split();
+                if (command[0] == "Add")
+                {
+                    wagons.Add(int.Parse(command[1]));
+                }
+                else
+                {
+                    int passengersToadd = int.Parse(comm);
+                    for(int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i] + passengersToadd <= maxPassengers)
+                        {
+                            wagons[i] += passengersToadd;
+                            break;
+                        }
+                    }
+                }
+                comm = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join(" ", wagons));
+        }
+
+       
+    }
+}
